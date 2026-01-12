a2c3a475e1022a9047c371fec4bd46782dfb73df
KarolVSDev <vieirakarol302@gmail.com>
2026-01-12 13:16:42 -0400

    first commit

---
+++ src/Firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY--lullQ_o4akQuUxcVVH32uwFPbkz1c",
+  authDomain: "ecommerce-7be71.firebaseapp.com",
+  projectId: "ecommerce-7be71",
+  storageBucket: "ecommerce-7be71.appspot.com",
+  messagingSenderId: "1024356890245",
+  appId: "1:1024356890245:web:8a8a8a8a8a8a8a8a8a8a8"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
